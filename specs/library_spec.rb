8bbb58f314054f8c527aca00d0b8882c526a6eea
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-04-02 22:18:43 +0100

    auto indented library

--- specs/library_spec.rb
+++ specs/library_spec.rb
@@ -4,112 +4,112 @@ require_relative('../library.rb')
 
 class TestLibrary < MiniTest::Test
 
-def setup
+  def setup
 
-@library = Library.new([
-  {
-  title: "the_northern_lights",
-  rental_details: {
-   student_name: "Graeme",
-   date: "03/04/18"
- }
-},
+    @library = Library.new([
+      {
+        title: "the_northern_lights",
+        rental_details: {
+          student_name: "Graeme",
+          date: "03/04/18"
+        }
+      },
 
-{
-  title: "the_subtle_knife",
-  rental_details: {
-   student_name: "Janice",
-   date: "15/04/18"
-  }
-},
-  {
-    title: "the_amber_spyglass",
-    rental_details: {
-     student_name: "Lindsay",
-     date: "12/04/18"
-    }
-  }
-])
+      {
+        title: "the_subtle_knife",
+        rental_details: {
+          student_name: "Janice",
+          date: "15/04/18"
+        }
+      },
+      {
+        title: "the_amber_spyglass",
+        rental_details: {
+          student_name: "Lindsay",
+          date: "12/04/18"
+        }
+      }
+      ])
 
-end
+    end
 
-def test_get_books()
+    def test_get_books()
 
-assert_equal([
-  {
-  title: "the_northern_lights",
-  rental_details: {
-   student_name: "Graeme",
-   date: "03/04/18"
- }
-},
+      assert_equal([
+        {
+          title: "the_northern_lights",
+          rental_details: {
+            student_name: "Graeme",
+            date: "03/04/18"
+          }
+        },
 
-{
-  title: "the_subtle_knife",
-  rental_details: {
-   student_name: "Janice",
-   date: "15/04/18"
-  }
-},
-  {
-    title: "the_amber_spyglass",
-    rental_details: {
-     student_name: "Lindsay",
-     date: "12/04/18"
-    }
-  }
-], @library.get_books())
+        {
+          title: "the_subtle_knife",
+          rental_details: {
+            student_name: "Janice",
+            date: "15/04/18"
+          }
+        },
+        {
+          title: "the_amber_spyglass",
+          rental_details: {
+            student_name: "Lindsay",
+            date: "12/04/18"
+          }
+        }
+        ], @library.get_books())
 
-end
+      end
 
-def test_get_book_info
+      def test_get_book_info
 
-  assert_equal({ title: "the_amber_spyglass",
-    rental_details: {
-     student_name: "Lindsay",
-     date: "12/04/18"
-    }}, @library.get_book_info("the_amber_spyglass"))
+        assert_equal({ title: "the_amber_spyglass",
+          rental_details: {
+            student_name: "Lindsay",
+            date: "12/04/18"
+            }}, @library.get_book_info("the_amber_spyglass"))
 
-  end
+          end
 
-def test_get_rental_details
+          def test_get_rental_details
 
-  assert_equal({
-   student_name: "Lindsay",
-   date: "12/04/18"
-  }, @library.get_rental_details("the_amber_spyglass"))
+            assert_equal({
+              student_name: "Lindsay",
+              date: "12/04/18"
+              }, @library.get_rental_details("the_amber_spyglass"))
 
-  end
-def test_add_new_book_to_library()
-  new_book = {title: "La Belle Sauvage", rental_details: { student_name:"", date: ""}}
-@library.add_new_book_to_library(new_book)
-assert_equal([
-  {
-  title: "the_northern_lights",
-  rental_details: {
-   student_name: "Graeme",
-   date: "03/04/18"
- }
-},
+            end
+            def test_add_new_book_to_library()
+              new_book = {title: "La Belle Sauvage", rental_details: { student_name:"", date: ""}}
+              @library.add_new_book_to_library(new_book)
+              assert_equal([
+                {
+                  title: "the_northern_lights",
+                  rental_details: {
+                    student_name: "Graeme",
+                    date: "03/04/18"
+                  }
+                },
 
-{
-  title: "the_subtle_knife",
-  rental_details: {
-   student_name: "Janice",
-   date: "15/04/18"
-  }
-},
-  {
-    title: "the_amber_spyglass",
-    rental_details: {
-     student_name: "Lindsay",
-     date: "12/04/18"
-    }
-  }, {title: "La Belle Sauvage", rental_details: { student_name:"", date: ""}}], @library.get_books)
-end
+                {
+                  title: "the_subtle_knife",
+                  rental_details: {
+                    student_name: "Janice",
+                    date: "15/04/18"
+                  }
+                },
+                {
+                  title: "the_amber_spyglass",
+                  rental_details: {
+                    student_name: "Lindsay",
+                    date: "12/04/18"
+                  }
+                  }, {title: "La Belle Sauvage", rental_details: { student_name:"", date: ""}}], @library.get_books)
+                end
 
-def test_change_rental_details()
-  @library.change_rental_details("the_subtle_knife", "Karen", "18/04/18")
-assert_equal({title: "the_subtle_knife", rental_details: { student_name:"Karen", date: "18/04/18"}}, @library.get_book_info("the_subtle_knife"))
-end
-end
+                def test_change_rental_details()
+                  @library.change_rental_details("the_subtle_knife", "Karen", "18/04/18")
+                  assert_equal({title: "the_subtle_knife", rental_details: { student_name:"Karen", date: "18/04/18"}}, @library.get_book_info("the_subtle_knife"))
+                end
+              end
