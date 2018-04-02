eb518918dfe8b8ebaee4b0b48f0dd26df31a5cce
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-04-02 20:40:53 +0100

    get books

---
+++ library.rb
@@ -0,0 +1,11 @@
+class Library
+
+  def initialize(library)
+    @library = library
+  end
+
+  def get_books
+    return @library
+  end
+
+end
--- specs/library_spec.rb
+++ specs/library_spec.rb
@@ -2,11 +2,11 @@ require('minitest/autorun')
 require('minitest/rg')
 require_relative('../library.rb')
 
-class Library < MiniTest::Test
+class TestLibrary < MiniTest::Test
 
 def setup
-@library = Library.new(
-  [
+
+@library = Library.new([
   {
   title: "the_northern_lights",
   rental_details: {
@@ -33,4 +33,31 @@ def setup
 
 end
 
+def test_get_books
+
+assert_equal([
+  {
+  title: "the_northern_lights",
+  rental_details: {
+   student_name: "Graeme",
+   date: "03/04/18"
+ }
+},
+
+{
+  title: "the_subtle_knife",
+  rental_details: {
+   student_name: "Janice",
+   date: "15/04/18"
+  }
+},
+  {
+    title: "the_amber_spyglass",
+    rental_details: {
+     student_name: "Lindsay",
+     date: "12/04/18"
+    }
+  }
+], @library.get_books)
+end
 end
